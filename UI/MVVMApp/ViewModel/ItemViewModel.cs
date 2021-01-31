79df50d5576cd63326709ac38c10fc9079d6ea38
HuyVo <huy.vminh71@gmail.com>
2021-01-31 12:43:44 +0200

    Temporary disable async method because of deadlock

--- UI/MVVMApp/ViewModel/ItemViewModel.cs
+++ UI/MVVMApp/ViewModel/ItemViewModel.cs
@@ -17,20 +17,30 @@ namespace MVVMApp.ViewModel
 
         public ItemViewModel()
         {
-           LoadContentsAsync();
-           
+            //var task = LoadContentsAsync();
+            //task.Wait();
+            //_Items = task.Result;
+            List<Item> items = new List<Item>();
+            items.Add(new Item(1, "Milk", 300, "liters"));
+            items.Add(new Item(2, "Carrot", 5, "kg"));
+            items.Add(new Item(3, "Potatoes", 3, "kg"));
+            _Items = items;
         }
-        public async void LoadContentsAsync()
-        {
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("http://localhost:5000/strgv1/get/all");
-            if (response.IsSuccessStatusCode)
-            {
-                string responseString = await response.Content.ReadAsStringAsync();
-                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(responseString);
-                _Items = items;                
-            }
-        }
+        
+        //DEADLOCK here when running async function sync
+
+        //public async Task<List<Item>> LoadContentsAsync()
+        //{
+        //    List<Item>items = new List<Item>();
+        //    HttpClient client = new HttpClient();
+        //    HttpResponseMessage response = await client.GetAsync("http://localhost:5000/strgv1/get/all");
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string responseString = await response.Content.ReadAsStringAsync();
+        //        items = JsonConvert.DeserializeObject<List<Item>>(responseString);
+        //    }
+        //    return items;
+        //}
 
         public Item Item
         {
